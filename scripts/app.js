5bc5e56b63cadcd68aceb63a3155f9e2ac67700e
Faisal M <mr.FaisalMohamoud@gmail.com>
2018-06-14 14:28:48 +0200

    Add - Modules bare-bones

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,16 @@
-  
-  
+"user-strict";
+
+//DATA CONTROLLER
+let dataController = (function() {})();
+
+// UI CONTROLLER
+let UIController = (function() {
+    
+   
+})();
+    
+// APP CONTROLLER
+let AppController = (function(){
+
+   
+})();
